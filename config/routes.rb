5e06ca16075a4b4ac9251f1c1ac11a5373e332d5
Henry van Wagenberg <Henryvw@Henrys-MacBook-Air.local>
2013-03-13 17:51:08 -0600

    updates with Will Sommers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Henryblog::Application.routes.draw do
+  get "admin/dashboard"
+
   resources :posts
 
 
-  get "home/index"
+  # get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +55,7 @@ Henryblog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
