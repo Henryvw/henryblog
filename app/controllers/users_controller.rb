7cf49751566c12da60d9580b69873c763b74d2a3
Henry van Wagenberg <henryvw@gmail.com>
2013-08-12 09:13:26 -0600

    fixing git and heroku

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+  private
+    def users_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+end end
