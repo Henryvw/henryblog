c0613fc337de373de6dce858ae848114e26cee9d
Henry van Wagenberg <henryvw@gmail.com>
2013-08-12 14:32:50 -0600

    precompile

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# force Rails into production mode when                          
+# you don't control web/app server and can't set it the proper way  
+ENV['RAILS_ENV'] ||= 'production'
+
 # Initialize the rails application
 Henryblog::Application.initialize!
