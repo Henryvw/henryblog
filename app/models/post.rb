79e67055085324d622658072f1721623d0a14fe1
Henryvw <Henryvw@gmail.com>
2014-02-22 16:33:10 +0100

    working with Will on config initialize update in order to get S3 working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
-  has_attached_file :watercolor, :styles => {:large => "450x450>", :medium => "375x375>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :watercolor,
+    :styles => {
+      :large => "450x450>",
+      :medium => "375x375>",
+      :thumb => "100x100>"
+      },
+    :default_url => "/images/:style/missing.png"
 
   acts_as_taggable
 
