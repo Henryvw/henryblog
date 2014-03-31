8bb61c1d2c369f75477f0de0326c87362df19f91
Henryvw <Henryvw@gmail.com>
2014-03-31 12:07:17 +0200

    changed to sessions instead of using login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def require_admin_user!
+    unless @current_user
+      redirect_to new_session_url
+      return false
+    end
+  end
+
   helper_method :current_user
 end
+
